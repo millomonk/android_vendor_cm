0ee9d3a0be29ca787046966cba773d720b53c89e
Schubi <schubi@erlangen.ccc.de>
2013-10-20 17:37:18 +0200

    Revert "Remove Term, su, and CMUpdater from user builds"
    This reverts commit a21887e8785851b0a71b55551430fe0ae6d76f8c.
    
    Conflicts:
    	config/common.mk
    
    Change-Id: I504f8eeabb0907e04d463bcaaeffc16148465d0a

--- config/common.mk
+++ config/common.mk
@@ -70,7 +70,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+    ro.build.selinux=1 \
+    persist.sys.root_access=1
 
 # Disable excessive dalvik debug messages
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -116,6 +117,11 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/compcache:system/bin/compcache \
     vendor/cm/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
 
+# Terminal Emulator
+PRODUCT_COPY_FILES +=  \
+    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
@@ -144,7 +150,9 @@ include vendor/cm/config/themes_common.mk
 PRODUCT_PACKAGES += \
     Development \
     LatinIME \
-    BluetoothExt
+    Superuser \
+    BluetoothExt \
+    su
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
@@ -196,14 +204,7 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat \
     ntfsfix \
-    ntfs-3g \
-    gdbserver \
-    micro_bench \
-    oprofiled \
-    procmem \
-    procrank \
-    sqlite3 \
-    strace
+    ntfs-3g
 
 # Openssh
 PRODUCT_PACKAGES += \
@@ -218,31 +219,6 @@ PRODUCT_PACKAGES += \
 # rsync
 PRODUCT_PACKAGES += \
     rsync
-
-# These packages are excluded from user builds
-ifneq ($(TARGET_BUILD_VARIANT),user)
-
-PRODUCT_PACKAGES += \
-    # CMUpdater \
-    Superuser \
-    su
-
-# Terminal Emulator
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=1
-else
-
-PRODUCT_PACKAGES += \
-    CMFota
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=0
-
-endif
 
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
